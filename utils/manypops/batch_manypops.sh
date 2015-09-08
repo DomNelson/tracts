80d0eb0bf834fd587e6a81db9c194c4968f279f4
DomNelson <nelson.dominic@gmail.com>
2015-09-08 18:58:31 -0400

    Increase number of populations simulated

--- utils/manypops/batch_manypops.sh
+++ utils/manypops/batch_manypops.sh
@@ -1,19 +1,25 @@
 set -e
 
-./sim_manypops.sh ~/project/tracts/sims/data/simon_batch/Argentina_mig 154 ~/project/tracts/sims/results/simon_batch/newmatrices/ Argentina 100 &
+./sim_manypops.sh ~/project/tracts/sims/data/simon_batch/Argentina_mig 154 ~/project/tracts/sims/results/simon_batch/newmatrices/1000pops/ Argentina 1000 &
 
 sleep 2
 
-./sim_manypops.sh ~/project/tracts/sims/data/simon_batch/Chile_mig 27 ~/project/tracts/sims/results/simon_batch/newmatrices/ Chile 100 &
+./sim_manypops.sh ~/project/tracts/sims/data/simon_batch/Chile_mig 27 ~/project/tracts/sims/results/simon_batch/newmatrices/1000pops/ Chile 1000 &
 
 sleep 2
 
-./sim_manypops.sh ~/project/tracts/sims/data/simon_batch/Colombia_mig 95 ~/project/tracts/sims/results/simon_batch/newmatrices/ Colombia 100 &
+./sim_manypops.sh ~/project/tracts/sims/data/simon_batch/Colombia_mig 95 ~/project/tracts/sims/results/simon_batch/newmatrices/1000pops/ Colombia1 500 &
+
 
 sleep 2
 
-./sim_manypops.sh ~/project/tracts/sims/data/simon_batch/Ecuador_mig 19 ~/project/tracts/sims/results/simon_batch/newmatrices/ Ecuador 100 &
+./sim_manypops.sh ~/project/tracts/sims/data/simon_batch/Colombia_mig 95 ~/project/tracts/sims/results/simon_batch/newmatrices/1000pops/ Colombia2 500 &
+
 
 sleep 2
 
-./sim_manypops.sh ~/project/tracts/sims/data/simon_batch/Peru_mig 117 ~/project/tracts/sims/results/simon_batch/newmatrices/ Peru 100 &
+./sim_manypops.sh ~/project/tracts/sims/data/simon_batch/Ecuador_mig 19 ~/project/tracts/sims/results/simon_batch/newmatrices/1000pops/ Ecuador 1000 &
+
+sleep 2
+
+./sim_manypops.sh ~/project/tracts/sims/data/simon_batch/Peru_mig 117 ~/project/tracts/sims/results/simon_batch/newmatrices/1000pops/ Peru 1000 &
